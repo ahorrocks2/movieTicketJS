107912a0652904e9723b056e3256c8c7e4d94b8e
JuliannaGreen & Alyssa Horrocks <student@epicodus.com>
2015-09-15 11:24:05 -0700

    Add Ticket constructor and cost function to determine price of given ticket

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,35 @@ function Movie(title, times) {
   this.title = title;
   this.times = times;
 };
+
+function Ticket(movie, time, age) {
+  this.movie = movie;
+  this.time = time;
+  this.age = age;
+};
+
+Ticket.prototype.cost = function() {
+  var total = 0
+  if (this.time < 5) {
+    total += 5;
+  }
+  else {
+    total += 10;
+  };
+
+  if (this.age === "Senior") {
+    total -= 2;
+  }
+
+  else if (this.age === "Child") {
+    total -= 3;
+  };
+
+  return total;
+};
+
+var newMovie = new Movie("Miss America", [1, 2, 5, 7, 12])
+var newMovie2 = new Movie("Things Change", [3, 4, 6, 10])
+var newMovie3 = new Movie("All About My Mother", [2, 3, 4, 6])
+var newMovie4 = new Movie("Henry Fool", [1, 3, 6])
+var newMovie5 = new Movie("Dead Man", [1, 4, 8])
--- spec/specs.js
+++ spec/specs.js
@@ -1,7 +1,21 @@
 describe('Movie', function() {
   it("creates a new movie with a list of times it plays", function() {
-    var testMovie = new Movie("Pulp Fiction", ["12", "2", "5", "11"]);
+    var testMovie = new Movie("Pulp Fiction", [1, 2, 5, 11]);
     expect(testMovie.title).to.equal("Pulp Fiction");
-    expect(testMovie.times).to.eql(["12", "2", "5", "11"]);
+    expect(testMovie.times).to.eql([1, 2, 5, 11]);
+  });
+});
+
+describe('Ticket', function() {
+  it("creates a new ticket associated with specific movie", function() {
+    var testTicket = new Ticket("Pulp Fiction", 5, "Adult");
+    expect(testTicket.movie).to.equal("Pulp Fiction");
+    expect(testTicket.time).to.equal(5);
+    expect(testTicket.age).to.equal("Adult");
+  });
+
+  it("calculate the cost of a ticket based on the ticket", function() {
+    var testTicket = new Ticket("Pulp Fiction", 5, "Adult");
+    expect(testTicket.cost()).to.equal(10);
   });
 });
